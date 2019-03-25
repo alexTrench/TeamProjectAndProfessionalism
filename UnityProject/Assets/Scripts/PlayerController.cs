b8e0b0b2f9c4e419e43b697c3eb8e682e577bd4a
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-03-25 10:09:47 +0000

    Added basic character movement

---
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/**
+ * @brief Controls a character with user input.
+ * @extends MonoBehaviour
+ * @date 25/03/2019
+ * @version 1.0 - 25/03/2019
+ */
+public class PlayerController : MonoBehaviour
+{
+    //[movementSpeed] How quickly the player is moving.
+    [SerializeField] private int movementSpeed;
+
+    /**
+     * Updates the player once every frame.
+     */
+    void Update()
+    {
+        //Move the player.
+        if(InputManager.Forward()) {
+            transform.position += transform.forward / movementSpeed;
+        }
+        if(InputManager.Backward()) {
+            transform.position += -transform.forward / movementSpeed;
+        }
+        if(InputManager.Right()) {
+            transform.position += transform.right / movementSpeed;
+        }
+        if(InputManager.Left()) {
+            transform.position += -transform.right / movementSpeed;
+        }
+    }
+}
