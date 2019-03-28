acfedc31f4b8984b9d08e1597a04a3b7c0f579be
alexTrench <35532782+alexTrench@users.noreply.github.com>
2019-03-28 00:06:49 +0000

    Merged My stuff To Main
    Weapons Added
    Modern Weapon Pack
    Game Controller

---
+++ UnityProject/Assets/GameControllers/TargetPractice.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPractice : MonoBehaviour
+{
+    public float health = 100;
+
+
+    private void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+}
