6f8a96d370f48819f90c73adfd76be6b079972c7
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-04-10 11:17:49 +0100

    create game manager and wave classes

--- UnityProject/Assets/Scripts/GamePlay/GameplayManager.cs
+++ UnityProject/Assets/Scripts/GamePlay/GameplayManager.cs
@@ -1,18 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/**
+ * @brief   Manages the games gameplay.
+ * @extends MonoBehaviour
+ * @author  Andrew Alford
+ * @date    10/04/19
+ * @version 1.0 - 10/04/19
+ */
 public class GameplayManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public static GameplayManager GM;
+
+    //[COOLDOWN] The amount of time (in seconds) 
+    //allocated between waves.
+    private const float COOLDOWN = 30.0f;
+
+    //[currentWave] The wave currently in progress.
+    private Wave currentWave = null;
+
+    /**
+     * @brief runs immediatly to make sure there 
+     *        is only one instance of this class.
+     */
+    void Awake() {
+        //DECLARE AS SINGLETON...
+
+        //Code to make the Game Manager a singleton.
+        if (!GM) {
+            //If the game manager does not yet exist,
+            //assign it to this instance.
+            DontDestroyOnLoad(gameObject);
+            GM = this;
+        }
+        else if (GM != this) {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    /**
+     * @brief Starts up the Gameplay Manager.
+     */
+    private void Start() {
+        StartWave();
     }
+
+    private void StartWave() {
+        currentWave = new Wave();
+    }
+
+    private void EndWave() {
+
+    }
+
+    //@reutrns the current wave in progress.
+    public Wave GetWave() => currentWave;
+
+
 }
---
+++ UnityProject/Assets/Scripts/Gameplay/Wave.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+/**
+ * @brief   Manages a wave.
+ * @extends MonoBehaviour
+ * @author  Andrew Alford
+ * @date    10/04/19
+ * @version 1.0 - 10/04/19
+ */
+public class Wave
+{
+    //[totalWaves] How many waves have occurred so far.
+    private static int totalWaves = 0;
+
+    //[BASE_ENEMIES] A base used to calculate how many 
+    //enemies to spawn in the wave.
+    private const int BASE_ENEMIES = 10;
+
+    //[DIMINISH_RETURNS] diminishes the returns 
+    //calculated from base variables.
+    private const float DIMINISH_RETURNS = 2.5f;
+
+    //[numEnemies] How many enemies are currently on the map
+    private int numEnemies;
+
+    //[waveNo] The wave that is currently in session.
+    private int waveNo;
+
+    /**
+     * @brief Constructor for a Wave.
+     */
+    public Wave() {
+        //Update the wave number.
+        waveNo = ++totalWaves;
+
+        //Calculate the number of enemies to spawn in this wave.
+        numEnemies = (int)(waveNo * (BASE_ENEMIES / DIMINISH_RETURNS));
+
+        Debug.Log("num enemies:\t" + numEnemies);
+    }
+}
