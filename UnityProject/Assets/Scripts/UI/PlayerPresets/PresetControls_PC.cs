1c9aa425f1ca495c1d1722eaaa00ce45e97d8206
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-04-22 11:23:30 +0100

    changed input fields to buttons

---
+++ UnityProject/Assets/Scripts/UI/PlayerPresets/PresetControls_PC.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * @brief   Allows the user to select different 
+ *          control presets (for PC)
+ * @author  Andrew Alford
+ * @date    22/04/19
+ * @version 1.0 - 22/04/19
+ */
+public class PresetControls_PC : MonoBehaviour
+{
+    //[presetSelect] A drop down to select presets.
+    [SerializeField] private Dropdown presetSelect = null;
+
+    //@brief Sets up the Preset Controls
+    private void Start() {
+        FindPresets();
+    }
+
+    /**
+     * @brief Searches through the player's preferences
+     *        to find the presets they have previously 
+     *        saved.
+     */
+    private void FindPresets() {
+        //[numPresets] How many presets the player has 
+        //previously saved.
+        int numPresets = PlayerPrefs.GetInt("numPresets", 0);
+
+        //[presetOptions] Holds the file names of the 
+        //player's preferred prests.
+        List<string> presetOptions = new List<string>();
+
+        //Add the defualt preset.
+        presetOptions.Add("defualt");
+        presetOptions.Add("test 1");
+        presetOptions.Add("test 2");
+
+        //Find all other presets.
+        for (int i = 0; i < numPresets; i++) {
+            //[presetName] Stores the name of the preset.
+            string presetName = PlayerPrefs.GetString("preset" + i, null);
+
+            //Add the preset.
+            if(!presetName.Equals(null)) {
+                presetOptions.Add(presetName);
+            }
+        }
+
+        //Add all options to the dropdown list.
+        if(presetSelect != null && presetSelect.isActiveAndEnabled) {
+            presetSelect.ClearOptions();
+            presetSelect.AddOptions(presetOptions);
+        }
+    }
+}
