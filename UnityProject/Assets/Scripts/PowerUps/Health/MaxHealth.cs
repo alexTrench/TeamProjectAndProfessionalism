a6d8296be3dfb18936a820f31ca13e0e8e16a580
CarlPendleton <35924791+CarlPendleton@users.noreply.github.com>
2019-04-23 22:13:46 +0100

    Added character switching sound effect and power-up assets

---
+++ UnityProject/Assets/Scripts/PowerUps/Health/MaxHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxHealth : MonoBehaviour
+{
+
+    public GameObject pickupEffect;
+
+
+    private void Start()
+    {
+      
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Player>().IncrementHealth(100);
+            Instantiate(pickupEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ UnityProject/Assets/Scripts/PowerUps/Weapon PowerUps/rocketTimed.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocketTimed : MonoBehaviour
+{
+
+    private GameObject previousWeapon;
+    public GameObject pickupEffect;
+    [SerializeField]
+    private int duration = 2;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Pickup(other));
+        }
+    }
+
+
+    IEnumerator Pickup(Collider player)
+    {
+       
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+
+        //disable the power up object on the level so we cannot collide with it again till it destroys itself later
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+       
+        Destroy(gameObject);
+    }
+}
